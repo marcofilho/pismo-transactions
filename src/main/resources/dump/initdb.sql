906911b458dcdcd4faf7f5d4142b415b1244ac05
Marco Antonio Barcellos Lima Filho <m.barceloslima@gmail.com>
2024-06-20 19:05:11 -0300

    adjust docker-compose.yaml and initdb.sql

---
+++ src/main/resources/dump/initdb.sql
@@ -0,0 +1,42 @@
+CREATE
+DATABASE PismoFinanceDB;
+
+USE
+PismoFinanceDB;
+
+DROP TABLE IF EXISTS `accounts`;
+CREATE TABLE `accounts`
+(
+    `account_id`      BIGINT      NOT NULL AUTO_INCREMENT,
+    `document_number` VARCHAR(50) NOT NULL,
+    PRIMARY KEY (`account_id`)
+);
+
+DROP TABLE IF EXISTS `operationsTypes`;
+CREATE TABLE `operationsTypes`
+(
+    `operationType_id` BIGINT       NOT NULL AUTO_INCREMENT,
+    `description`      VARCHAR(100) NOT NULL,
+    PRIMARY KEY (`operationType_id`)
+);
+
+DROP TABLE IF EXISTS `transactions`;
+CREATE TABLE `transactions`
+(
+    `transaction_id`   BIGINT         NOT NULL AUTO_INCREMENT,
+    `account_id`       BIGINT         NOT NULL,
+    `operationType_id` BIGINT         NOT NULL,
+    `amount`           DECIMAL(10, 2) NOT NULL,
+    `eventDate`        DATETIME       NOT NULL,
+    PRIMARY KEY (`transaction_id`),
+    FOREIGN KEY (`account_id`) REFERENCES accounts (`account_id`),
+    FOREIGN KEY (`operationType_id`) REFERENCES operationsTypes (`operationType_id`)
+);
+
+INSERT INTO `operationsTypes` (`operation_type_id`, `description`)
+VALUES (1, 'COMPRA A VISTA'),
+       (2, 'COMPRA PARCELADA'),
+       (3, 'SAQUE'),
+       (4, 'PAGAMENTO');
+
+COMMIT;
