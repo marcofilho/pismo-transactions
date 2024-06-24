6fb4cfdb4b2ed3732a560c244bed86b1d8af6dfc
Marco Antonio Barcellos Lima Filho <m.barceloslima@gmail.com>
2024-06-24 20:57:21 -0300

    Swagger documentation and config

--- src/main/java/com/pismo/payment/transactions/controllers/AccountController.java
+++ src/main/java/com/pismo/payment/transactions/controllers/AccountController.java
@@ -1,15 +1,18 @@
 package com.pismo.payment.transactions.controllers;
 
-import com.pismo.payment.transactions.domain.account.Account;
 import com.pismo.payment.transactions.dtos.in.AccountDTO;
 import com.pismo.payment.transactions.dtos.out.AccountResponse;
 import com.pismo.payment.transactions.services.AccountService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/accounts")
+@Api(value = "Account Controller", tags = {"Accounts"}, description = "Account API's")
 public class AccountController {
 
     private final AccountService accountService;
@@ -19,7 +22,9 @@ public class AccountController {
     }
 
     @PostMapping(value = "")
-    public ResponseEntity<AccountResponse> createAccount(@RequestBody AccountDTO accountDto) throws Exception {
+    @ApiOperation(value = "Create a new account", notes = "This API creates one new account with the data requested.")
+    public ResponseEntity<AccountResponse> createAccount(
+            @ApiParam(value = "Dados da nova conta", required = true) @RequestBody AccountDTO accountDto) throws Exception {
         var account = accountService.createAccount(accountDto);
         return new ResponseEntity<>(AccountResponse.builder()
                 .accountId(account.getAccountId())
@@ -30,7 +35,9 @@ public class AccountController {
 
     @GetMapping(value = "/{account_id}")
     @ResponseBody
-    public ResponseEntity<AccountResponse> getAccountByAccountId(@PathVariable Long account_id) throws Exception {
+    @ApiOperation(value = "Get an account by an ID", notes = "This API returns the account details by the given ID.")
+    public ResponseEntity<AccountResponse> getAccountByAccountId(
+            @ApiParam(value = "ID da conta", required = true) @PathVariable Long account_id) throws Exception {
         var account = accountService.getAccountByAccountId(account_id);
         return new ResponseEntity<>(AccountResponse.builder()
                 .accountId(account.getAccountId())
--- src/main/java/com/pismo/payment/transactions/controllers/TransactionController.java
+++ src/main/java/com/pismo/payment/transactions/controllers/TransactionController.java
@@ -1,15 +1,18 @@
 package com.pismo.payment.transactions.controllers;
 
-import com.pismo.payment.transactions.domain.transaction.Transaction;
 import com.pismo.payment.transactions.dtos.in.TransactionDTO;
 import com.pismo.payment.transactions.dtos.out.TransactionResponse;
 import com.pismo.payment.transactions.services.TransactionService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/transactions")
+@Api(value = "Transaction Controller", tags = {"Transactions"}, description = "Transactions API's")
 public class TransactionController {
 
     private final TransactionService transactionService;
@@ -19,7 +22,9 @@ public class TransactionController {
     }
 
     @PostMapping(value = "")
-    public ResponseEntity<TransactionResponse> createTransaction(@RequestBody TransactionDTO transactionDTO) throws Exception {
+    @ApiOperation(value = "Create a new transaction", notes = "This API creates a new transaction with the data requested.")
+    public ResponseEntity<TransactionResponse> createTransaction(
+            @ApiParam(value = "Dados da nova transação", required = true) @RequestBody TransactionDTO transactionDTO) throws Exception {
         var transaction = transactionService.createTransaction(transactionDTO);
         return new ResponseEntity<>(TransactionResponse.builder()
                 .transactionId(transaction.getTransactionId())
