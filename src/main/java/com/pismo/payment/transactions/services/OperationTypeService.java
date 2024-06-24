2a7c26e015d9809b9a1fc043b5000e72f6a0f8db
Marco Antonio Barcellos Lima Filho <m.barceloslima@gmail.com>
2024-06-24 20:59:47 -0300

    Fix Dependency injection

--- src/main/java/com/pismo/payment/transactions/services/OperationTypeService.java
+++ src/main/java/com/pismo/payment/transactions/services/OperationTypeService.java
@@ -2,17 +2,21 @@ package com.pismo.payment.transactions.services;
 
 import com.pismo.payment.transactions.domain.operationType.OperationType;
 import com.pismo.payment.transactions.repositories.OperationTypeRepository;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-@AllArgsConstructor
 public class OperationTypeService {
-    @Autowired
+
     private OperationTypeRepository operationTypeRepository;
+
+    @Autowired
+    public OperationTypeService(OperationTypeRepository operationTypeRepository) {
+        this.operationTypeRepository = operationTypeRepository;
+    }
+
 
     public OperationType getOperationTypeByOperationTypeId(Long operationTypeId) throws Exception {
         return operationTypeRepository.findByOperationTypeId(operationTypeId).orElseThrow(() -> new Exception("OperationType invalid."));
